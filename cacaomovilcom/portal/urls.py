5f499fdb022a3c4bfa40ca56b293a687db9a316c
Javier Wilson <javier.wilson@gmail.com>
2016-05-19 20:32:36 +0000

    eventos en portada

--- cacaomovilcom/portal/urls.py
+++ cacaomovilcom/portal/urls.py
@@ -5,10 +5,12 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.views.generic import ListView
+
 from .models import *
+from .views import *
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^$', HomePageView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
     url(r'^eventos/$', ListView.as_view(model=Event), name='events'),
     url(r'^guias/$', ListView.as_view(model=Guide), name='guides'),
--- cacaomovilcom/portal/views.py
+++ cacaomovilcom/portal/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Event
+
+class HomePageView(TemplateView):
+
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['events'] = Event.objects.all()[:5]
+        return context
