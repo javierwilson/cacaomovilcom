3a59c95850f1dc741ddd9e742aa631d470fb3757
Javier Wilson <javier.wilson@gmail.com>
2017-04-24 20:57:39 +0000

    remove html

--- config/settings/production.py
+++ config/settings/production.py
@@ -61,7 +61,7 @@ SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=False)
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['beta.cacaomovil.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['.cacaomovil.com'])
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ('gunicorn', )
